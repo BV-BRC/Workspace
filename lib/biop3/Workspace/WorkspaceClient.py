8433bfac7ae7476490e881457706d543fce786ad
Robert Olson <olson@mcs.anl.gov>
2016-05-12 16:24:18 -0500

    Some code cleanup. Beginning of interactive workspace shell.

--- lib/biop3/Workspace/WorkspaceClient.py
+++ lib/biop3/Workspace/WorkspaceClient.py
@@ -109,7 +109,7 @@ class Workspace(object):
                  password=None, token=None, ignore_authrc=False,
                  trust_all_ssl_certificates=False):
         if url is None:
-            raise ValueError('A url is required')
+            url = 'http://p3.theseed.org/services/Workspace'
         scheme, _, _, _, _, _ = _urlparse.urlparse(url)
         if scheme not in _URL_SCHEME:
             raise ValueError(url + " isn't a valid http url")
